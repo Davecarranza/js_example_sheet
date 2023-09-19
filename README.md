07cbf2434193f34fc1c9547728cd7c4ee0b2eaa0
Dave Carranza <davecarranzabiz@gmail.com>
2023-09-19 11:22:39 -0400

    Just adding notes on JS fundamentals

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# js_example_sheet
+# js_example_sheet 
+
+This is just a follow along with the JS Fundamentals in The Odin Project. Tasking notes as I go along and playing with Git.
---
+++ javascript.js
@@ -0,0 +1,19 @@
+console.log(23 + 97);
+
+console.log(3 + 6 + 9 + 3 + 6 + 9);
+
+console.log((4 + 6 + 9) / 77); 
+
+let a = 10;
+
+console.log(a);
+
+console.log(9 * a); 
+
+let b = 7 * a;
+
+console.log(b);
+
+const max = 57;
+let actual = max - 13;
+let percentage = actual / max; 
